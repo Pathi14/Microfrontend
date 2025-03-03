3c85f5c1c8f131447d91755c92f56a120644d7f2
pathi14 <pathipayong@gmail.com>
2025-03-03 13:15:10 +0100

    feat : implementation du header

---
+++ header/src/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+  return <h1>Micro Frontend Header ... Youpi!!!</h1>;
+};
+
+export default Header;
---
+++ header/webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  devServer: {
+    port: 3001,
+  },
+  mode: 'development',
+  output: {
+    publicPath: "http://localhost:3001/",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'header',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Header': './src/Header',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+};
